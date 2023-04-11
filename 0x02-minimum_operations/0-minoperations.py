5dfe37ce665a41886589d926a5e3761cf3fc4478
Nonsi <debockannaline@gmail.com>
2023-04-11 04:02:03 +0200

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,47 @@
 #!/usr/bin/python3
+'''module contains solution to min-operations problem'''
+import math
 
-"""
-    Method that determines the number of minmum operations given n characters
-"""
+
+def highestPrimeFactor(n):
+    '''finds highest prime factor of n'''
+    max_prime = 0
+
+    while n % 2 == 0:
+        max_prime = 2
+        n = n / 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            max_prime = i
+            n = n / i
+
+    if n > 2:
+        max_prime = n
+
+    return int(max_prime)
+
+
+def isPrime(n):
+    ''' checks whether number is prime or not '''
+    flag = False
+
+    if n > 1:
+        for i in range(2, n):
+            if (n % i) == 0:
+                flag = True
+                break
+    return not flag
 
 
 def minOperations(n):
-    """
-        A function that calculates the fewest number of operations
-        needed to give a result of exactly n H characters in a file
-        args: n: Number of characters to be displayed
-        return:
-               number of min operations
-    """
+    ''' finds number of minimum of operations to get n number of H '''
+    if n <= 1:
+        return 0
 
-    now = 1
-    start = 0
-    counter = 0
-    while now < n:
-        remainder = n - now
-        if (remainder % now == 0):
-            start = now
-            now += start
-            counter += 2
-        else:
-            now += start
-            counter += 1
-    return 
+    if n == 2 or isPrime(n):
+        return n
+
+    hpf = highestPrimeFactor(n)
+    resultOfDivision = n / hpf
+    return int(hpf + resultOfDivision)
